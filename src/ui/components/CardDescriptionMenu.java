f0f01774485e98b96885630d9c7657562ca56d80
dasdwqdf <victormaringa21@gmail.com>
2024-11-29 14:26:08 -0300

    Adicionados Novos Sprites:  * direct-damage  * empty-mana  * filled-mana
    UI's para Status Novos:
     * mana
     * direct-damage

--- src/ui/components/CardDescriptionMenu.java
+++ src/ui/components/CardDescriptionMenu.java
@@ -30,49 +30,98 @@ public class CardDescriptionMenu extends Menu {
 
     public void draw(Graphics2D g2d) {
         drawMenuWindow(g2d);
-        drawCurrentSelectedCardDescription(g2d);
+        drawCurrentSelectedCard(g2d);
     }
 
-    public void drawCurrentSelectedCardDescription(Graphics2D g2d) {
+    public void drawCardStatus(Graphics2D g2d, Card currentSelectedCard) {
+        // Caso nulo
+        if (currentSelectedCard == null) {
+            return;
+        }
+
+        // Coordenadas
+        int tempX = menuX + gamePanel.tileSize/2;
+        int tempY = menuY + gamePanel.tileSize/2 + 4 * borderSize;
+
+        // Atributos de Poder e Dano Direto, para a carta do tipo POWER
+        if (currentSelectedCard.getCardType().equals(CardType.POWER)) {
+            // Poder da Carta
+            BufferedImage cardElementSprite = currentSelectedCard.getCardElementSprite();
+            g2d.drawImage(cardElementSprite, tempX, tempY - gamePanel.tileSize/2, gamePanel.tileSize/2, gamePanel.tileSize/2, null);
+
+            Color powerColor = switch(currentSelectedCard.getCardElement()) {
+                case FIRE -> new Color(255, 0, 0);
+                case WATER -> new Color(0, 0, 255);
+                case GRASS -> new Color(0,80,0);
+            };
+            String powerText = currentSelectedCard.getPower().toString();
+
+            g2d.setFont(menuFont);
+            g2d.setColor(powerColor);
+            g2d.drawString(powerText, tempX + gamePanel.tileSize/2 + 4, tempY);
+
+            // Dano Direto
+            tempX += (int) (menuWidth/3.5);
+            BufferedImage directDamageSprite = currentSelectedCard.getCardDirectDamageSprite();
+            g2d.drawImage(directDamageSprite, tempX, tempY - gamePanel.tileSize/2, gamePanel.tileSize/2, gamePanel.tileSize/2, null);
+
+            Color directDamageColor = new Color(225, 80, 20);
+            String directDamageText = String.valueOf(currentSelectedCard.getDirectDamage());
+
+            g2d.setColor(directDamageColor);
+            g2d.drawString(directDamageText, tempX + gamePanel.tileSize/2 + 4, tempY);
+
+            tempX += (int) (menuWidth/3.5);
+        }
+
+        // Custo de Mana
+        BufferedImage cardManaCostSprite = currentSelectedCard.getCardManaSprite();
+        g2d.drawImage(cardManaCostSprite, tempX, tempY - gamePanel.tileSize/2, gamePanel.tileSize/2, gamePanel.tileSize/2, null);
+
+        Color manaCostColor = new Color(0, 173, 201);
+        String manaCostText = String.valueOf(currentSelectedCard.getManaCost());
+
+        g2d.setColor(manaCostColor);
+        g2d.drawString(manaCostText, tempX + gamePanel.tileSize/2 + 4, tempY);
+    }
+
+    public void drawCardDescription(Graphics2D g2d, Card currentSelectedCard) {
+        // Caso nulo
+        if (currentSelectedCard == null) {
+            return;
+        }
+
+        // Coordenadas
+        int tempX = menuX + gamePanel.tileSize/2;
+        int tempY = menuY + gamePanel.tileSize + 4 * borderSize;
+
+        // Fonte e Cor dos Textos
+        g2d.setFont(cardDescriptionFont);
+        g2d.setColor(Color.black);
+
+        // Separamos a descrição em linhas de no máximo 16 caracteres
+        String[] linhas = Utils.breakString(currentSelectedCard.getDescription(), 16);
+
+        // Para cada linha desenhamos na tela sua respectiva String
+        for (String linha : linhas) {
+            g2d.drawString(linha, tempX , tempY);
+            tempY += gamePanel.tileSize/2;
+        }
+    }
+
+    public void drawCurrentSelectedCard(Graphics2D g2d) {
+        // Resgatamos a carta selecionada atualmente
         int currentIndex = battleMenuController.getCurrentSelectedCardIndex();
         Card currentSelectedCard = battleMenuController.getPlayerHand().get(currentIndex);
 
-        if (currentSelectedCard != null) {
-            // Fonte e Cor dos textos
-            g2d.setColor(Color.black);
-            g2d.setFont(cardDescriptionFont);
-            int x = menuX + gamePanel.tileSize/2;
-            int y = menuY + gamePanel.tileSize/2 + 4 * borderSize;
-
-            if (currentSelectedCard.getCardType().equals(CardType.POWER)) {
-                BufferedImage cardElementSprite = currentSelectedCard.getCardElementSprite();
-                g2d.drawImage(cardElementSprite, x, y - gamePanel.tileSize/2, gamePanel.tileSize/2, gamePanel.tileSize/2, null);
-
-                Color powerColor = switch(currentSelectedCard.getCardElement()) {
-                    case FIRE -> new Color(255, 0, 0);
-                    case WATER -> new Color(0, 0, 255);
-                    case GRASS -> new Color(0,80,0);
-                };
-
-                String powerText = currentSelectedCard.getPower().toString();
-
-                g2d.setFont(menuFont);
-                g2d.setColor(powerColor);
-                g2d.drawString(powerText, x + gamePanel.tileSize/2 + 4, y);
-
-                y += gamePanel.tileSize/2;
-            }
-
-            g2d.setFont(cardDescriptionFont);
-            g2d.setColor(Color.black);
-
-            String[] linhas = Utils.breakString(currentSelectedCard.getDescription(), 16);
-            for (String linha : linhas) {
-                g2d.drawString(linha, x , y);
-                y += gamePanel.tileSize/2;
-            }
-
+        // Caso nulo
+        if (currentSelectedCard == null) {
+            return;
         }
+
+        // Desenhamos as informações da Carta na tela
+        drawCardStatus(g2d, currentSelectedCard);
+        drawCardDescription(g2d, currentSelectedCard);
     }
 
 }
